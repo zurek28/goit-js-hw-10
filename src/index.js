52a561408773fe0820e0690e62b5978df8720d78
Kamil Zurawski <zurek93@gmail.com>
2023-01-25 17:02:00 +0100

    Hw10

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import './css/styles.css';
+import _ from 'lodash';
+import { Notify } from 'notiflix';
+
+const DEBOUNCE_DELAY = 500;
+
+let input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const searchFilter = {
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
+};
+
+input.addEventListener(
+  'input',
+  _.debounce(e => {
+    fetchJsonResponse(
+      `https://restcountries.com/v2/name/${e.target.value.trim()}?fields=${searchFilter[
+        'fields'
+      ].join()}`
+    ).then(response => {
+      if (response.length > 10) {
+        Notify.info(
+          'To many matches found. Please enter a more specific name.'
+        );
+      } else if (response.length < 10 && response.length > 1) {
+        htmlListInjection(response);
+      } else if (response.length === 1) {
+        countryList.innerHTML = '';
+        htmlInfoInjection(response);
+      }
+
+      if (response['status'] === 404) {
+        Notify.failure('Oops, there in no country with that name.');
+      }
+    });
+  }, DEBOUNCE_DELAY)
+);
+
+function fetchJsonResponse(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log('Error: ', error));
+}
+
+function htmlListInjection(countriesArray) {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  countriesArray.forEach(elem => {
+    const listLine = document.createElement('div');
+    listLine.classList.add('list-element');
+
+    listLine.innerHTML = `<li><img src="${elem['flags']['svg']}" alt="Flag of ${elem['name']}" height="16" width="22"/> ${elem['name']}</li>`;
+
+    countryList.append(listLine);
+  });
+}
+
+function htmlInfoInjection(countryArray) {
+  const singleCountry = countryArray[0];
+  const languages = singleCountry['languages'];
+  let languagesArray = [];
+
+  languages.forEach(elem => {
+    languagesArray.push(elem['name']);
+  });
+  countryInfo.innerHTML = `<h1><img src="${
+    singleCountry['flags']['svg']
+  }" alt="Flag of ${
+    singleCountry['name']
+  }" height="24" width="30" style="margin-right: 10px;"/>${
+    singleCountry['name']
+  }</h1>
+  <p><b>Capital:</b> ${singleCountry['capital']}</p>
+  <p><b>Population:</b> ${singleCountry['population']}</p>
+  <p><b>Launguages:</b> ${languagesArray.join(', ')}</p>`;
+}
